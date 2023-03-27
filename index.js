774c2c1c5824d21016d923d7a59dd64a409de2fe
UDsGitHub <64935889+UDsGitHub@users.noreply.github.com>
2023-03-26 19:42:42 -0500

    changed origin to true in index

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: true,
     credentials: true,
   })
 );
